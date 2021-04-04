4e29d701052266ea1995d6dbdae931616ebf81fd
WORTHY-CTO <hong2hwa58@gmail.com>
2021-04-04 18:55:51 +0900

    upload files

---
+++ React class/blog/src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import InterationSample  from './InterationSample';
+
+
+class App extends Component {
+  render() {
+    return <InterationSample />;
+  }
+}
+
+export default App;
